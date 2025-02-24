a452d9a369e888355118cf1480fabdee94e642b2
rgongw <smileeric0414@gmail.com>
2025-02-24 17:38:40 +0800

    v_destructor.cpp, v_function.cpp: why virtual destructors need implementation? It is because of v_table, and that the destructors would be called automatically after the derived destructor called

--- binding/v_destructor.cpp
+++ binding/v_destructor.cpp
@@ -9,14 +9,19 @@ public:
 };
 
 Base::~Base() {
-    // Implementation of pure virtual destructor
+    //-----------------------------------------------------
+    /* Unlike simple/pure virtual functions, pure virtual ***DESTRUCTORS*** must have a definition in the base class, even if it is pure.
+    This is mainly because of the v_table mechanism: The v_table contains the addresses of the virtual functions of the class.
+    Secondly, automatically, the derived class destructor will call the base class destructor, and if the base class destructor is not defined,
+    the derived class destructor will not be able to call it through the virtual table of base_obj. */
+   //-----------------------------------------------------
     std::cout << "Base class destructor" << std::endl;
 }
 
 class Derived : public Base {
 public:
     ~Derived() {
-        std::cout << "Derived class destructor" << std::endl; // Add implementation to Derived class destructor
+        std::cout << "Derived class destructor" << std::endl; // Implementation in Derived class destructor
     }
     void someFunction() {
         std::cout << "Function in Derived class" << std::endl;
@@ -24,7 +29,7 @@ public:
 };
     
 int main() {
-    Base* basePtr = new Derived();
+    Base* basePtr = new Derived(); // created on the heap, i.e., it is created at runtime.
     delete basePtr;
     return 0;
 }
---
+++ binding/v_function.cpp
@@ -0,0 +1,60 @@
+#include <iostream> 
+#include <string>
+
+class Shape {
+    static int nextID;
+    int id;
+public:
+    virtual void draw() const = 0; // pure virtual function
+    virtual void error(const std::string& msg) {
+        // (simple) virtual function: can be overridden in derived classes, if it's not overridden in derived classes, the base class function will be called, i.e., the interface is inherited, but the implementation is not.
+        // however, if this function is overridden in the derived class, the base class function will not be called, i.e. both interface and implementation are inherited.
+        std::cerr << "Shape Error: " << msg << std::endl;
+    } 
+    Shape() : id(nextID++) {}
+    virtual ~Shape() {}
+    int getObjectID() const { return id; } // non-virtual function: had better not overridden in derived classes, i.e., inherit both the interface and implementation, and *do not* modify the inherited implementation.
+}; // since there're pure virtual functions, this class is abstract and cannot be instantiated, but can be used as a base class for derived.
+
+int Shape::nextID = 1;
+
+
+class Rectangle: public Shape {
+
+    void draw() const override {
+        std::cout << "Drawing Rectangle" << std::endl;
+    }
+
+    void error(const std::string& msg) override {
+        std::cerr << "Rectangle Error: " << msg << std::endl;
+    }
+};
+
+class Ellipse: public Shape {
+
+    void draw() const override {
+        std::cout << "Drawing Ellipse" << std::endl;
+    }
+
+    void error(const std::string& msg) override {
+        std::cerr << "Ellipse Error: " << msg << std::endl;
+    }
+};
+
+int main() {
+    Shape* shapes[2];
+    shapes[0] = new Rectangle();
+    shapes[1] = new Ellipse();
+
+    for (int i = 0; i < 2; i++) {
+        std::cout << "Object ID: " << shapes[i]->getObjectID() << std::endl;
+        shapes[i]->draw();
+        // shapes[i]->error("Error");
+    }
+
+    for (int i = 0; i < 2; i++) {
+        delete shapes[i];
+    }
+
+    return 0;
+}
