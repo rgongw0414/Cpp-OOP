52ccda1e8ea9e8dd818c86c94d4c5ead0e21e242
rgongw <smileeric0414@gmail.com>
2025-03-02 23:39:16 +0800

    keyword/mutable.cpp: mutable keyword is mostly used for debugging

---
+++ keyword/mutable.cpp
@@ -0,0 +1,14 @@
+class Entity {
+public:
+    int getX() const {
+        m_getXCount_debug++;
+        return x;
+    }
+
+    int getDebugCount() const {
+        return m_getXCount_debug; // for the sake of debugging
+    }
+private:
+    mutable int m_getXCount_debug = 0; // mutable allows to modify a member in a const function
+    int x;
+};
